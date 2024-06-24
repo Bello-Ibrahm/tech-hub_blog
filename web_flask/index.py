d874f08f5a6b776ccf54a185b62e28d46f2a5878
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-06-24 18:25:03 +0100

    Implements necessary routes

--- web_flask/index.py
+++ web_flask/index.py
@@ -13,6 +13,7 @@ def close_db(error):
 
 
 @app.route('/', strict_slashes=False)
+@app.route('/home', strict_slashes=False)
 def index():
     """ TECH HUB BLOG is alive! """
     return render_template('index.html')
@@ -34,6 +35,42 @@ def register():
 def forgot_password():
     """ Handles forgot password """
     return render_template('forgot-password.html')
+
+
+@app.route('/dashboard', strict_slashes=False)
+def dashboard():
+    """ Handles admin dashboard """
+    return render_template('dashboard.html')
+
+
+@app.route('/user', strict_slashes=False)
+def user():
+    """ Handles User """
+    return render_template('user.html')
+
+
+@app.route('/post', strict_slashes=False)
+def post():
+    """ Handles Post """
+    return render_template('post.html')
+
+
+@app.route('/view-post', strict_slashes=False)
+def view_post():
+    """ Handles admin view post """
+    return render_template('view-post.html')
+
+
+@app.route('/category', strict_slashes=False)
+def category():
+    """ Handles Category """
+    return render_template('category.html')
+
+
+@app.route('/view-category', strict_slashes=False)
+def view_category():
+    """ Handles view Category """
+    return render_template('view-category.html')
 
 
 if __name__ == "__main__":
