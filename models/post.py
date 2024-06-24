a10079ffa2be950060ccbecf5fc92b6272246ae7
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-06-24 12:30:56 +0100

    Add middleware files

---
+++ models/post.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" holds class User"""
+
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+
+
+class Post(BaseModel, Base):
+    """Representation of a post """
+    
+    __tablename__ = 'posts'
--- models/user.py
+++ models/user.py
@@ -6,24 +6,12 @@ from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
-from hashlib import md5
 
 
 class User(BaseModel, Base):
     """Representation of a user """
     
     __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
-
-    def __setattr__(self, name, value):
-        """sets a password with md5 encryption"""
-        if name == "password":
-            value = md5(value.encode()).hexdigest()
-        super().__setattr__(name, value)
+    email = Column(String(150), nullable=False)
+    name = Column(String(150), nullable=False)
+    role = Column(String(10), nullable=False)
