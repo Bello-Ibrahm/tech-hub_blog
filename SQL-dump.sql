f598245420ae8eaddfd87bdb41299043efde52bc
Bello-Ibrahm <Ibrahimbello1243@gmail.com>
2024-06-24 09:38:40 +0100

    Add UI/UX files

--- SQL-dump.sql
+++ SQL-dump.sql
@@ -21,15 +21,56 @@ DROP DATABASE IF EXISTS tech_hub_blog_db;
 -- Create database + user if doesn't exist
 CREATE DATABASE IF NOT EXISTS tech_hub_blog_db;
 CREATE USER IF NOT EXISTS 'tech_hub_blog_dev'@'localhost';
--- SET PASSWORD FOR 'tech_hub_blog_dev'@'localhost' = 'tech_hub_blog_dev_pwd'; This line gives password error issues, so i used the below
-SET PASSWORD FOR 'tech_hub_blog_dev'@'localhost' = PASSWORD('tech_hub_blog_dev_pwd');
+-- Use ALTER USER instead of SET PASSWORD
+ALTER USER 'tech_hub_blog_dev'@'localhost' IDENTIFIED BY 'tech_hub_blog_dev_pwd';
 GRANT ALL ON tech_hub_blog_db.* TO 'tech_hub_blog_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'tech_hub_blog_dev'@'localhost';
 FLUSH PRIVILEGES;
 
-
-
 USE tech_hub_blog_db;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` varchar(60) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name` varchar(150) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `role` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `id` varchar(60) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name` varchar(128) NOT NULL,
+  `slug` varchar(250),
+  `description` TEXT NOT NULL,
+  `image` varchar(250) NOT NULL,
+  `meta_title` varchar(128) NOT NULL,
+  `meta_description` varchar(128) NOT NULL,
+  `meta_keyword` varchar(128) NOT NULL,
+  `navbar_status` TINYINT() default 0,
+  `status` TINYINT() default 0,
+  `created_by` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `posts`
@@ -43,9 +84,32 @@ CREATE TABLE `posts` (
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `name` varchar(128) NOT NULL,
-  `state_id` varchar(60) NOT NULL,
+  `slug` varchar(250),
+  `description` TEXT NOT NULL,
+  `yt_iframe` varchar(128) NOT NULL,
+  `meta_title` varchar(128) NOT NULL,
+  `meta_description` varchar(128) NOT NULL,
+  `meta_keyword` varchar(128) NOT NULL,
+  `status` TINYINT() default 0,
+  `created_by` varchar(128) NOT NULL,
+  `category_id` varchar(60) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `state_id` (`state_id`),
-  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`state_id`) REFERENCES `categories` (`id`)
+  KEY `category_id` (`category_id`),
+  KEY `created_by` (`created_by`),
+  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
+  CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
