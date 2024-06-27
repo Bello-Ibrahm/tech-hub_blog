52c77310b7829dffc60dbd1ebd55f8266331f17b
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-06-27 23:31:55 +0300

    Instances are not working of each class

--- models/user.py
+++ models/user.py
@@ -1,14 +1,35 @@
 from sqlalchemy import Column, String, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
+from datetime import datetime
+from sqlalchemy.orm import relationship
+import uuid
 
+Base = declarative_base()
 
-class User(BaseModel, Base):
-    """Representation of a user """
+class User(Base, BaseModel):
     __tablename__ = 'users'
-    email = Column(String(150), nullable=False)
-    name = Column(String(150), nullable=False)
-    role = Column(String(10), nullable=False)
+    
+    id = Column(String(150), primary_key=True, default=lambda: str(uuid.uuid4()))
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow, nullable=False, onupdate=datetime.utcnow)
+    username = Column(String(150), nullable=False)
+    email = Column(String(250), nullable=False)
+    image_file = Column(String(250), nullable=False, default='default.jpg')
+    password = Column(String(250), nullable=False)
+    role = Column(Integer, nullable=False)
+    posts = relationship('Post', backref='auther', lazy=True)
+    user = relationship('User', backref="posts", lazy=True)
+
+
+    # def __init__(self, username, email, image_file, password, role):
+    #     self.username = username
+    #     self.email = email
+    #     self.image_file = image_file
+    #     self.password = password
+    #     self.role = role
+    
+    
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
