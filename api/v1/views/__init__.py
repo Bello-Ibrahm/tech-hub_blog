dfc65ce33ddcc8cb17648a5f3ae427b5ad20131d
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-06-27 21:25:52 +0300

    DB is now connected with the models

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,10 +7,5 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.users import *
-<<<<<<< HEAD
 from api.v1.views.posts import *
 from api.v1.views.categories import *
-=======
-from api.v1.views.post import *
-from api.v1.views.category import *
->>>>>>> 405d5c8 (DB is now connected with the models)
