4a9b82ba1f1daba8fc7ee527f4eb773805a9464d
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-06-29 02:09:12 +0300

    edits in post.py file

--- models/post.py
+++ models/post.py
@@ -4,16 +4,24 @@
 
 from models.base_model import BaseModel, Base
 import sqlalchemy
+<<<<<<< HEAD
 from sqlalchemy import Column, String, Integer, Text, ForeignKey, DateTime
 from datetime import datetime
 from sqlalchemy.orm import relationship
 
+=======
+from datetime import datetime
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Integer, Text, ForeignKey, DateTime
+from datetime import datetime
+>>>>>>> 43f724e (edits in post.py file)
 
 
 class Post(BaseModel, Base):
     """Representation of a post """
     __tablename__ = 'posts'
     
+<<<<<<< HEAD
     id = Column(Integer, primary_key=True)
     title = Column(String(100), nullable=False)
     date_posted = Column(DateTime, nullable=False, default=datetime.utcnow)
@@ -24,5 +32,26 @@ class Post(BaseModel, Base):
     #relationship
     category = relationship('Category', backref='posts', lazy=True)
     
+=======
+
+    title = Column(String(100), nullable=False)
+    date_posted = Column(DateTime, nullable=False, default=datetime.utcnow)
+    content = Column(Text, nullable=False)
+    user_id = Column(String(150), ForeignKey('users.id'), nullable=False)  # assuming user_id is varchar(150)
+    category_id = Column(String(150), ForeignKey('categories.id'), nullable=True)  # assuming category_id is varchar(150)
+
+    # Relationships
+    category = relationship('Category', backref='posts', lazy=True)
+    user = relationship('User', backref='user_posts', lazy=True)
+    user_id = Column(ForeignKey('users.id'), nullable=False)
+    users = relationship("User", backref="post", foreign_keys="User.post_id")
+    post_id = Column(ForeignKey('posts.id'), nullable=False)
+    user = relationship('User', backref="posts", lazy=True)
+
+
+    #relationship
+    category = relationship('Category', backref='posts', lazy=True)
+
+>>>>>>> 43f724e (edits in post.py file)
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
