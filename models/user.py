8a78ce8ad0f5578e419fae917cde91e2027fbadb
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-06-28 12:15:33 +0100

    Modify the user model

--- models/user.py
+++ models/user.py
@@ -5,13 +5,25 @@ import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer
+from hashlib import md5
 
 
 class User(BaseModel, Base):
     """Representation of a user """
     
     __tablename__ = 'users'
-    email = Column(String(150), nullable=False)
     name = Column(String(150), nullable=False)
-    role = Column(String(10), nullable=False)
+    email = Column(String(150), nullable=False)
+    password = Column(String(128), nullable=False)
+    role = Column(Integer, default=0, nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
+
+    def __setattr__(self, name, value):
+        """sets a password with md5 encryption"""
+        if name == "password":
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
