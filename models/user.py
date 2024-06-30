503b18ab06a272355a90cf48d835fa994ba94812
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-06-30 10:09:37 +0100

    Implement the user model

--- models/user.py
+++ models/user.py
@@ -6,7 +6,7 @@ from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String, Integer
-from hashlib import md5
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -14,9 +14,11 @@ class User(BaseModel, Base):
     
     __tablename__ = 'users'
     name = Column(String(150), nullable=False)
-    email = Column(String(150), nullable=False)
+    email = Column(String(150), nullable=False, unique=True)
     password = Column(String(128), nullable=False)
     role = Column(Integer, default=0, nullable=False)
+    # posts = relationship('Post', backref='author', lazy=True)
+    # categories = relationship('Category', backref='author', lazy=True)
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
@@ -25,5 +27,9 @@ class User(BaseModel, Base):
     def __setattr__(self, name, value):
         """sets a password with md5 encryption"""
         if name == "password":
-            value = md5(value.encode()).hexdigest()
+            value = hashlib.md5(value.encode()).hexdigest()
         super().__setattr__(name, value)
+
+    def verify_password(self, entered_password):
+        entered_md5_hash = hashlib.md5(entered_password.encode()).hexdigest()
+        return entered_md5_hash == self.password
