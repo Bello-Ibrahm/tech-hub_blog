0b720efa7238de5e7c9be323ab6fe3360441fe29
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-06-30 10:12:51 +0100

    Implements the get user record by email method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -88,6 +88,18 @@ class DBStorage:
             if (value.id == id):
                 return value
 
+    def get_by_email(self, cls, email):
+        """
+        Returns the object based on the class name and its Email, or
+        None if not found
+        """
+        if cls not in classes.values():
+            return None
+
+        all_cls = models.storage.all(cls)
+        for value in all_cls.values():
+            if (value.email == email):
+                return value
         return None
 
     def count(self, cls=None):
