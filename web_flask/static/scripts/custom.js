fdd61bf05e0bc459b0bb67e211cdb3e8a361be3a
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-07-03 12:24:51 +0100

    Add custom script file

---
+++ web_flask/static/scripts/custom.js
@@ -0,0 +1,27 @@
+$(function () {
+    // CodeMirror
+    CodeMirror.fromTextArea(document.getElementById("codeMirrorTextDesc"), {
+        mode: "htmlmixed",
+        lineNumbers: true,
+        theme: "monokai"
+    });
+});
+
+// Initialize CodeMirror
+// var editor = CodeMirror.fromTextArea(document.getElementById("codeMirrorTextDesc"), {
+//     mode: "htmlmixed",
+//     lineNumbers: true,
+//     theme: "default"
+// });
+
+$(function () {
+    $("#example1").DataTable({
+        "responsive": true, "lengthChange": false, "autoWidth": false,
+        "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+});
+
+$(function () {
+    //Add text editor
+    $('#post-textarea').summernote()
+});
