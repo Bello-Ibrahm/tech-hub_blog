4f3f6cd65179874d4019056103064d99ec23ad4d
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-07-04 19:44:08 +0300

    Chore: index.py is a new file to be rended as the home page with a new code replaced by the old index.py however the old html file of index still exist but under the name of mainindex.html

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 from models import storage
 from api.v1.views import app_views
 from flask import jsonify
+from .categories import get_categories
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -14,9 +15,11 @@ def status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def number_objects():
     """ Retrieves the number of each objects by type """
+    cats = get_categories()
     data = {
         "users": storage.count("User"),
-        "categories": storage.count("Category"),
+        # "categories": storage.count("Category"),
+        "categories": cats,
         "posts": storage.count("Post"),
     }
     res = jsonify(data)
