08e98226e568861444531ba2b331f06354cf0caa
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-07-07 17:27:21 +0300

    Refactor: Remove unused error handling code in app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,7 +21,15 @@ def close_db(error):
     storage.close()
 
 
-
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 app.config['SWAGGER'] = {
     'title': 'Tech-Hub Blog Restful API',
