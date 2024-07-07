d806ab95a5a0cfa864ce13932e197c4ce08c652e
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-07-07 16:32:07 +0300

    Add: category.py table has been created - application is not yet running properly

--- models/category.py
+++ models/category.py
@@ -8,3 +8,20 @@ from sqlalchemy import Column, String, Integer, Text, ForeignKey, BLOB
 from sqlalchemy.orm import relationship
 from slugify import slugify # to handle the slugs
 
+
+class Category(BaseModel, Base):
+    """Representation of a category """
+    
+    __tablename__ = 'categories'
+    name = Column(String(150), nullable=False, unique=True)
+    slug = Column(String(250), nullable=False)
+    description = Column(Text, nullable=False)
+    image = Column(String(250), nullable=True)
+    meta_title = Column(String(200), nullable=False)
+    meta_description = Column(String(200), nullable=False)
+    meta_keyword = Column(String(200), nullable=False)
+    navbar_status = Column(Integer, default=0, nullable=False)
+    status = Column(Integer, default=0, nullable=False)
+    # created_by = Column(String(150), nullable=False)
+    created_by = Column(String(150), ForeignKey('users.id'), nullable=False)
+    creator = relationship('User', backref='categories')
