f0db8d593f258b355712ee7ec2c32464dd0f5b6c
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-07-07 17:20:30 +0300

    Refactor: Remove unused import in index.py

--- web_flask/index.py
+++ web_flask/index.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 import secrets
 from PIL import Image
 from flask import (
-    Flask, render_template, redirect,
+    Flask, redirect,
     flash, url_for, request, session
 )
 from dotenv import load_dotenv
