cfc9501e7215d5c7fd7a6aefb9cf925de8325a5d
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-07-07 16:38:20 +0300

    Establish relationship with User model via created_by foreign key Implement __init__ method to initialize Category instances Override __setattr__ method to handle slug creation using slugify

--- models/category.py
+++ models/category.py
@@ -6,12 +6,12 @@ from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, String, Integer, Text, ForeignKey, BLOB
 from sqlalchemy.orm import relationship
-from slugify import slugify # to handle the slugs
+from slugify import slugify  # to handle the slugs
 
 
 class Category(BaseModel, Base):
     """Representation of a category """
-    
+
     __tablename__ = 'categories'
     name = Column(String(150), nullable=False, unique=True)
     slug = Column(String(250), nullable=False)
@@ -25,3 +25,13 @@ class Category(BaseModel, Base):
     # created_by = Column(String(150), nullable=False)
     created_by = Column(String(150), ForeignKey('users.id'), nullable=False)
     creator = relationship('User', backref='categories')
+
+    def __init__(self, *args, **kwargs):
+        """initializes category"""
+        super().__init__(*args, **kwargs)
+
+    def __setattr__(self, name, value):
+        """ Set a slug with slugify """
+        if name == "slug":
+            value = slugify(value)
+        super().__setattr__(name, value)
